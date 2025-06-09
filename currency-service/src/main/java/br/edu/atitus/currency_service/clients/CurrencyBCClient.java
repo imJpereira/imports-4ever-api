e1b3db661754959d77f70c2449b7ac59457c9e22
João <jps.pereira0805@gmail.com>
2025-06-08 21:40:29 -0300

    primeiro commit

---
+++ currency-service/src/main/java/br/edu/atitus/currency_service/clients/CurrencyBCClient.java
@@ -0,0 +1,16 @@
+package br.edu.atitus.currency_service.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "bancoCentralClient",
+            url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata",
+            fallback = CurrencyBCFallback.class)
+public interface CurrencyBCClient {
+
+    @GetMapping("/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda='{moeda}'&@dataCotacao='05-16-2025'&$format=json")
+    CurrencyBCReponse getCurrencyBC(
+            @PathVariable String moeda
+    );
+}
---
+++ product-service/src/main/java/br/edu/atitus/productservice/controllers/OpenProductController.java
@@ -0,0 +1,70 @@
+package br.edu.atitus.productservice.controllers;
+
+import br.edu.atitus.productservice.clients.CurrencyClient;
+import br.edu.atitus.productservice.clients.CurrencyResponse;
+import br.edu.atitus.productservice.entities.ProductEntity;
+import br.edu.atitus.productservice.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
+public class OpenProductController {
+
+    private final ProductRepository productRepository;
+    private final CurrencyClient currencyClient;
+    private final CacheManager cacheManager;
+
+    public OpenProductController(ProductRepository productRepository, CurrencyClient currencyClient, CacheManager cacheManager) {
+        this.productRepository = productRepository;
+        this.currencyClient = currencyClient;
+        this.cacheManager = cacheManager;
+    }
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @GetMapping("/{idProduct}/{targetCurrency}")
+    public ResponseEntity<ProductEntity> getById(@PathVariable Long idProduct, @PathVariable String targetCurrency) throws Exception {
+
+        targetCurrency = targetCurrency.toUpperCase();
+        String nameCache = "Product";
+        String keyCache = idProduct + "_" + targetCurrency;
+
+        ProductEntity product = cacheManager.getCache(nameCache).get(keyCache, ProductEntity.class);
+
+        if (product == null) {
+            product = productRepository.findById(idProduct)
+                    .orElseThrow(() -> new Exception("Product not found"));
+
+            product.setEnvironment("" + serverPort);
+
+            if (targetCurrency.equals(product.getCurrency())) {
+                product.setConvertedPrice(product.getPrice());
+            } else {
+                CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(), product.getCurrency(), targetCurrency);
+
+                if (currency != null) {
+                    product.setEnvironment("service: product - port: " + product.getEnvironment() + " // " + currency.getEnvironment());
+                    product.setConvertedPrice(currency.getConvertedValue());
+                } else {
+                    product.setConvertedPrice(-1);
+                    product.setEnvironment("service: product - port: " + product.getEnvironment() + " // Currency unavailable");
+                }
+            }
+
+            cacheManager.getCache(nameCache).put(keyCache, product);
+        } else {
+            product.setEnvironment("service: product - port:" + serverPort + " (cache) // " + targetCurrency);
+        }
+
+        return ResponseEntity.ok(product);
+    }
+
+
+}
