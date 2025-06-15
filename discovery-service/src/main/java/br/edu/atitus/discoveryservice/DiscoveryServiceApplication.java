7cc4bf86855bf3f7fe70a46dfd7712f6cfade567
João <jps.pereira0805@gmail.com>
2025-06-14 23:10:23 -0300

    create table no currency e no product

--- discovery-service/src/main/java/br/edu/atitus/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/br/edu/atitus/discoveryservice/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package br.edu.atitus.dicoveryservice;
+package br.edu.atitus.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DicoveryServiceApplication {
+public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DicoveryServiceApplication.class, args);
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
 }
---
+++ product-service/src/main/resources/db/migration/V1__CreateTableProduct.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tb_products (
+    id UUID PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    url VARCHAR(255) NOT NULL,
+    value NUMERIC(14,2) NOT NULL,
+    discount_value NUMERIC(14,2),
+    status VARCHAR(10) NOT NULL CHECK (status IN ('ATIVO', 'INATIVO')),
+    team UUID,
+    category UUID,
+    sport UUID,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+);
