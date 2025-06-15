42e2cd2fe80f810f97e262dca271906336adfcda
João <jps.pereira0805@gmail.com>
2025-06-15 14:25:24 -0300

    crud completo

---
+++ team-service/src/main/java/br/edu/atitus/teamservice/TeamServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.teamservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamServiceApplication.class, args);
+    }
+
+}
