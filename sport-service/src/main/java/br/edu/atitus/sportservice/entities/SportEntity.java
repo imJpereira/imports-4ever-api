1fa172f0041ca8811011a363bccfcb3521e64d01
Bruno <bruno.pf2005@gmail.com>
2025-06-20 19:07:52 -0300

    Criação do sport-service

---
+++ sport-service/src/main/java/br/edu/atitus/sportservice/entities/SportEntity.java
@@ -0,0 +1,46 @@
+package br.edu.atitus.sportservice.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "sports_tb")
+public class SportEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", unique = true, nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+}
